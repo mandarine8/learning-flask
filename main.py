9a68d79d10ffe88b4f7dbd8cfcf235f429b2a2f4
Amandine Car <caramandine@gmail.com>
2020-03-27 19:32:53 -0400

    comments added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from app import app, db
 from app.models import User, Post
 
+# Set context in console to avoid import before, flask shell instead python
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Post': Post}
