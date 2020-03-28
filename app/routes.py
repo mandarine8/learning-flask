ec2c270b5626350436ae4526ebeeca8cb12289c6
Amandine Car <caramandine@gmail.com>
2020-03-28 01:04:33 -0400

    require to login and redirection with next

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,13 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
+from flask_login import login_required
+from flask import request
+from werkzeug.urls import url_parse
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
   user = {'username': 'Amandine'}
   posts = [
@@ -22,7 +26,14 @@ def index():
 def login():
   form = LoginForm()
   if form.validate_on_submit():
-    flash('Login requested for user {}, remember_me={}'.format(
-      form.username.data, form.remember_me.data))
-    return redirect(url_for('index'))
+    user = User.query.filter_by(username=form.username.data).first()
+    if user is None or not user.check_password(form.password.data):
+      flash('Invalid username or password')
+      return redirect(url_for('login'))
+    login_user(user, remember=form.remember_me.data)
+    next_page = request.args.get('next')
+    if not next_page or url_parse(next_page).netloc != '':
+      next_page = url_for('index')
+    return redirect(next_page)
   return render_template('login.html', title='Sign In', form=form)
+
